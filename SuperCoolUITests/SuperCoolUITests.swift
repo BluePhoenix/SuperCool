436e73255c72f9aae4c6cac10d087316624c4beb
Felix Barros <felix.barros@gmail.com>
2015-11-24 22:56:13 -0500

    Add Make Me Super Cool button and UI test

--- SuperCoolUITests/SuperCoolUITests.swift
+++ SuperCoolUITests/SuperCoolUITests.swift
@@ -28,9 +28,11 @@ class SuperCoolUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testSuperCoolButton() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        XCTAssert(app.buttons["Make Me Super Cool"].exists)
     }
     
 }
